3c0f32d88990bc52688817af7b34726c32e75bbe
oneraner <oneraner@gmail.com>
2020-02-10 02:18:23 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+    center: [22.604799,120.2976256],
+    zoom: 3
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);

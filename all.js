dae805dbbd47ecd7e0e181bf8a84b78f70959c4f
oneraner <oneraner@gmail.com>
2020-02-12 15:10:39 +0800

    Add files via upload

--- all.js
+++ all.js
@@ -15,8 +15,19 @@ var greenIcon = new L.Icon({
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
   });
+
+var redIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
   
-  var markers = new L.MarkerClusterGroup().addTo(map);;
+  
+var markers = new L.MarkerClusterGroup().addTo(map);;
+
 
 var xhr = new XMLHttpRequest();
 xhr.open("get","https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json");
@@ -24,9 +35,16 @@ xhr.send();
 xhr.onload = function(){
  var data = JSON.parse(xhr.responseText).features
  for(let i=0;data.length>i;i++){
+    var mask;
+    if(data[i].properties.mask_abult == 0){
+        mask = redIcon;}
+    else{
+        mask = greenIcon;
+    }
     markers.addLayer(L.marker([data[i].geometry.coordinates[1],
-    data[i].geometry.coordinates[0]], {icon:greenIcon}).bindpopup(
-    data[i].properties.name));
+    data[i].geometry.coordinates[0]], {icon:mask})
+    .bindPopup('<h1>'+data[i].properties.name+'</h1>'+'<p>成人口罩數量'+data[i].properties.mask_adult+'</p>')
+);
  }
  map.addLayer(markers);
 }
